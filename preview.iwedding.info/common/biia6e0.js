fa26f1cb18c02ea30ceb9b819f19d9183f9dbcbf
DungttTran <112999427+DungttTran@users.noreply.github.com>
2024-03-19 14:17:18 +0700

    edit biia6e0.js

--- preview.iwedding.info/common/biia6e0.js
+++ preview.iwedding.info/common/biia6e0.js
@@ -179,7 +179,7 @@ if(biicore.bgMusic){
 	}
 	</style>
 	<div class="bii-player">
-		<div id="play-audio" onclick="playPause();" class="bii-player-secondary"><div class="bii-player-secondary-content"></div></div>
+		<div id="play-audio" onclick="playPause();" class="bii-player-secondary"></div>
 		<div onclick="playPause();" class="playerIcon">
 			<span id="playerVolumeOff">
 				<svg xmlns="http://www.w3.org/2000/svg" width="25" height="27" fill="#fff" class="bi bi-volume-mute-fill" viewBox="0 0 16 16">
